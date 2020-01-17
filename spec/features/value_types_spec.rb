566ba105184edd5c0a5f877e5c7650efc5c0fc9e
Rustam Ibragimov <iamdaiver@gmail.com>
2020-01-17 03:00:42 +0300

    [specs] Value::Text

---
+++ spec/features/value_types_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe 'SmartCore::Types::Value types' do
+  describe 'Text' do
+    specify 'validation' do
+      expect(SmartCore::Types::Value::Text.valid?('test')).to eq(true)
+      expect(SmartCore::Types::Value::Text.valid?(:test)).to eq(true)
+      expect(SmartCore::Types::Value::Text.valid?(123)).to eq(false)
+      expect(SmartCore::Types::Value::Text.valid?(Object.new)).to eq(false)
+    end
+
+    specify 'type-casting' do
+      expect(SmartCore::Types::Value::Text.cast('test')).to eq('test')
+      expect(SmartCore::Types::Value::Text.cast(:test)).to eq('test')
+      expect(SmartCore::Types::Value::Text.cast([])).to eq('[]')
+      expect(SmartCore::Types::Value::Text.cast([])).to eq('{}')
+    end
+  end
+end
