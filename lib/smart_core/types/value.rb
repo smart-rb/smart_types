a47249adb159ab61d4d6b7f3e38b91be97644f30
Rustam Ibragimov <iamdaiver@gmail.com>
2020-01-25 01:34:56 +0300

    [value] require nil type

--- lib/smart_core/types/value.rb
+++ lib/smart_core/types/value.rb
@@ -3,6 +3,7 @@
 # @api public
 # @since 0.1.0
 class SmartCore::Types::Value < SmartCore::Types::Primitive
+  require_relative 'value/nil'
   require_relative 'value/string'
   require_relative 'value/symbol'
   require_relative 'value/text'
