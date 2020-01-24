99a635b313a2e976a362d18065c830a0a2b4c561
Rustam Ibragimov <iamdaiver@gmail.com>
2020-01-25 01:33:29 +0300

    [value] Nil type

---
+++ lib/smart_core/types/value/nil.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+SmartCore::Types::Value.define_type(:Nil) do |type|
+  type.define_checker do |value|
+    # rubocop:disable Style/NilComparison
+    value == nil # NOTE: #nil? is not used cuz BasicObject has no #nil? method
+    # rubocop:enable Style/NilComparison
+  end
+end
