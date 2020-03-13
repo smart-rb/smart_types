f9e643a79441bf91525af0bacb778c5258556bff
Rustam Ibragimov <iamdaiver@gmail.com>
2020-03-14 00:04:48 +0300

    [code] better code style

--- lib/smart_core/types/value/text.rb
+++ lib/smart_core/types/value/text.rb
@@ -3,5 +3,8 @@
 # @api public
 # @since 0.1.0
 SmartCore::Types::Value::Text = SmartCore::Types::System.type_sum(
-  SmartCore::Types::Value::String, SmartCore::Types::Value::Symbol
-) { |type| type.define_caster(&:to_s) }
+  SmartCore::Types::Value::String,
+  SmartCore::Types::Value::Symbol
+) do |type|
+  type.define_caster(&:to_s)
+end
