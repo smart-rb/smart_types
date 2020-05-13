876468e311186999d9c8d121a2e56a580b256e47
Rustam Ibragimov <iamdaiver@gmail.com>
2020-05-13 19:25:57 +0300

    [types/value/unbound_method] skeleton

---
+++ lib/smart_core/types/value/unbound_method.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# @api public
+# @since 0.2.0
+SmartCore::Types::Value.define_type(:UnboundMethod) do |type|
+  type.define_checker do |value|
+    # TODO: realize
+  end
+end
