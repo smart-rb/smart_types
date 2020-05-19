ac6c8fc44fc9692de65850bd81b7bf4233a78b6b
Rustam Ibragimov <iamdaiver@gmail.com>
2020-05-19 22:17:34 +0300

    [types/value/chain] enumerator chain skeleton

---
+++ lib/smart_core/types/value/enumerator_chain.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# @api public
+# @since 0.2.0
+SmartCore::Types::Value.define_type(:EnumeratorChain) do |type|
+  type.define_checker do |value|
+    # TODO: realize
+  end
+
+  type.define_caster do |value|
+    # TODO: realize
+  end
+end
