ea448146d15ee71d7830ee95aba9190f322572ed
Rustam Ibragimov <iamdaiver@gmail.com>
2020-05-21 14:25:09 +0300

    [types/string_io] skeleton

---
+++ lib/smart_core/types/value/string_io.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# frozen_string_literal: true
+
+# @api public
+# @since 0.2.0
+SmartCore::Types::Value.define_type(:StringIO) do |type|
+  type.define_checker do |value|
+    # TODO: realize
+  end
+
+  type.define_caster do |value|
+    # TODO: realize
+  end
+end
