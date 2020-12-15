d87a942929891dc2888efd51007d1964b900b367
Rustam Ibragimov <iamdaiver@gmail.com>
2020-12-15 03:45:18 +0300

    [value/set] pending specs (specs skeleton)

--- spec/types/value/set_spec.rb
+++ spec/types/value/set_spec.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
 RSpec.describe 'SmartCore::Types::Value::Set' do
+  context 'non-nilable type' do
+    let(:type) { SmartCore::Types::Value::Set }
+
+    xspecify 'type-casting'
+    xspecify 'type-checking'
+    xspecify 'type-validation'
+  end
+
+  context 'runtime-based non-nilable type' do
+    let(:type) { SmartCore::Types::Value::Set() }
+
+    xspecify 'type-casting'
+    xspecify 'type-checking'
+    xspecify 'type-validation'
+  end
+
+  context 'nilable type' do
+    let(:type) { SmartCore::Types::Value::Set.nilable }
+
+    xspecify 'type-casting'
+    xspecify 'type-checking'
+    xspecify 'type-validation'
+  end
+
+  context 'runtime-based nilable type' do
+    let(:type) { SmartCore::Types::Value::Set().nilable }
+
+    xspecify 'type-casting'
+    xspecify 'type-checking'
+    xspecify 'type-validation'
+  end
+
+  specify 'has no support for runtime attributes' do
+    expect { SmartCore::Types::Value::Set(Set.new([1,2,3])) }.to raise_error(
+      SmartCore::Types::RuntimeAttriburtesUnsupportedError
+    )
+  end
 end
