a22998ee7a84d62f6be5dfb8eebaf7613bb5192f
basherru <basherru@umbrellio.biz>
2021-02-05 19:16:29 +0300

    update version

--- lib/smart_core/types/value/date_time.rb
+++ lib/smart_core/types/value/date_time.rb
@@ -6,7 +6,7 @@ using SmartCore::Ext::BasicObjectAsObject
 
 # @api public
 # @since 0.1.0
-# @version 0.5.0
+# @version 0.6.0
 SmartCore::Types::Value.define_type(:DateTime) do |type|
   type.define_checker do |value|
     value.is_a?(::DateTime) || value == ::DateTime::Infinity
